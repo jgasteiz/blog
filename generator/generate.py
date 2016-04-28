d870c467d60b6a24a487dd6201ea8ac18ec9e1eb
Javi Manzano <javi.manzano.oller@gmail.com>
2016-04-28 21:45:20 +0100

    Add script for generating the blog

---
+++ generator/generate.py
@@ -0,0 +1,19 @@
+from generator import BlogGenerator
+from settings import (
+    BASE_PATH,
+    CONTENT_PATH,
+    OUTPUT_PATH,
+    PAGE_SIZE,
+)
+
+
+def main():
+    BlogGenerator(
+        base_path=BASE_PATH,
+        content_path=CONTENT_PATH,
+        output_path=OUTPUT_PATH,
+        page_size=PAGE_SIZE,
+    ).generate()
+
+if __name__ == '__main__':
+    main()
---
+++ generator/settings.py
@@ -0,0 +1,4 @@
+BASE_PATH = './base'
+CONTENT_PATH = './blog_posts'
+OUTPUT_PATH = './public'
+PAGE_SIZE = 3
