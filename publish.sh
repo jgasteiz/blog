a978b43b67eede5ad24d166312d27989fe7dd91e
Javi Manzano <javi.manzano@octoenergy.com>
2024-10-28 11:27:08 +0100

    Publish

--- publish.sh
+++ publish.sh
@@ -2,11 +2,9 @@
 
 # Generate the site
 python ./generator/generate.py
-
-# Publish the site, using today's date as the commit message
-message="Publish $(date + '%Y-%m-%d %H:%M:%S')"
+date=$(date + '%Y-%m-%d %H:%M:%S')
 git add --all
-git commit -m $message
+git commit -m "Publish $date"
 git push origin master
 cd ./public
 git add --all
